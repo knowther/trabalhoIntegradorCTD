4008cf93130f567e2ef0038991b7143b143d88b3
Johnny Wesley <johnnywesley.contato@outlook.com>
2022-11-10 20:56:44 -0300

    commit inicial

---
+++ trabalhoIntegrador/src/main/java/com/dh/trabalhoIntegrador/TrabalhoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.dh.trabalhoIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIntegradorApplication.class, args);
+	}
+
+}
---
+++ trabalhoIntegrador/src/test/java/com/dh/trabalhoIntegrador/TrabalhoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.trabalhoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

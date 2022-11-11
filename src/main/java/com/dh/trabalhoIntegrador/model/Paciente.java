90f0ed59483ed66a97a48bb7f7395ce7a96acc8b
Johnny Wesley <johnnywesley.contato@outlook.com>
2022-11-10 21:24:38 -0300

    paiente add

--- src/main/java/com/dh/trabalhoIntegrador/model/Paciente.java
+++ src/main/java/com/dh/trabalhoIntegrador/model/Paciente.java
@@ -1,4 +1,51 @@
 package com.dh.trabalhoIntegrador.model;
 
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
 public class Paciente {
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private LocalDate dataCadastro;
+
+    public Paciente(String nome, String sobrenome, String rg, LocalDate dataCadastro) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
 }

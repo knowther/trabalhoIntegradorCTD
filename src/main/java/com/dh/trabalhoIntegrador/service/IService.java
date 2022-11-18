ff49477fa8dfedd4c1e368731f471128415d121a
Knowther <johnnywesley.contato@outlook.com>
2022-11-17 21:52:27 -0300

    service

--- src/main/java/com/dh/trabalhoIntegrador/service/IService.java
+++ src/main/java/com/dh/trabalhoIntegrador/service/IService.java
@@ -1,5 +1,6 @@
 package com.dh.trabalhoIntegrador.service;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IService<T> {
@@ -7,4 +8,7 @@ public interface IService<T> {
     public T salvar (T t);
 
     public Optional<T> buscar (Integer id);
+
+    public List<T> buscarTodos();
+
 }
--- src/main/java/com/dh/trabalhoIntegrador/service/impl/PacienteService.java
+++ src/main/java/com/dh/trabalhoIntegrador/service/impl/PacienteService.java
@@ -2,9 +2,15 @@ package com.dh.trabalhoIntegrador.service.impl;
 
 import com.dh.trabalhoIntegrador.model.Paciente;
 import com.dh.trabalhoIntegrador.service.IService;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PacienteService implements IService<Paciente> {
 
     @Override
@@ -14,6 +20,11 @@ public class PacienteService implements IService<Paciente> {
 
     @Override
     public Optional<Paciente> buscar(Integer id) {
-        return Optional.empty();
+        return Optional.of(new Paciente("Johnny", "Wesley", "123.123.134", LocalDate.now()));
+    }
+
+    @Override
+    public List<Paciente> buscarTodos() {
+        return Arrays.asList(new Paciente("Johnny", "Wesley", "123.123.134", LocalDate.now()));
     }
 }
